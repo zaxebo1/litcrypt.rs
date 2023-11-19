afb7df18e43b5418895e844a9ee7f019ab347602
Kudaes <kurosh_2907@hotmail.com>
2023-11-19 16:28:43 +0100

    Update crate's documentation

--- src/litcrypt.rs
+++ src/litcrypt.rs
@@ -1,61 +1,64 @@
-//! # LitCrypt
-//! The name is an abbreviation of ‘Literal Encryption’ – a Rust compiler plugin to encrypt
-//! text literals using the [XOR cipher](https://en.wikipedia.org/wiki/XOR_cipher).
-//!
-//! LitCrypt let’s you hide your static string literal in the binary from naughty eyes and protect
-//! your app from illegal cracking activity.
-//!
-//! LitCrypt works by encrypting string literals during compile time. An encrypted string remains
-//! encrypted both on disk and in memory during runtime. It is decypted only when used.
-//!
-//! ## Usage
-//! In `Cargo.toml`, add:
-//!
-//! ```toml
+//! LITCRYPT2 
+//! ===========
+//! 
+//! It's a short name of "Literal Encryption", a Rust proc macro that encrypts text using a basic XOR method. It protect plain text from static analysis tools and helps keep your important app safe from cracking activity.
+//! 
+//! LITCRYPT2 encrypts strings when compiling, keeping them encrypted in both disk and memory while running, and only decrypting them when needed.
+//! 
+//! This crate is just a maintained and updated fork of the original crate, **LITCRYPT** by **Robin Syihab (r@ansvia.com)**.
+//! 
+//! USAGE
+//! -----
+//! 
+//! Dependencies:
+//! 
+//! ```rust
 //! [dependencies]
-//! litcrypt = "0.2"
+//! litcrypt2 = "0.1"
 //! ```
-//!
-//! # Example
-//!
+//! 
+//! Example:
+//! 
 //! ```rust
 //! #[macro_use]
-//! extern crate litcrypt;
-//!
-//! use_litcrypt!("MY-SECRET-SPELL");
-//!
-//! fn main(){
+//! extern crate litcrypt2;
+//! 
+//! use_litcrypt!();
+//! 
+//! fn main()
+//! {
 //!     println!("his name is: {}", lc!("Voldemort"));
 //! }
+//! 
+//! fn raw_string()
+//! {
+//!     println!("The command line console can be found in the path {}", lc!(r"C:\Windows\System32\cmd.exe"));
+//! }
 //! ```
-//!
-//! The [`use_litcrypt!`] macro must be called first, for initialization. Its parameter is the
-//! secret key that is used to encrypt all [`lc!`]-wrapped string literal(s).
-//! This key is also encrypted and will not visible in a static analyzer.
-//!
-//! Only after that can you use the [`lc!`] macro.
-//!
-//! You can also override the key using an environment variable `LITCRYPT_ENCRYPT_KEY` e.g:
-//! ```bash
-//! ❯ export LITCRYPT_ENCRYPT_KEY="myverysuperdupermegaultrasecretkey"
-//! ```
-//!
-//! LitCrypt will statically encrypt every string encapsulated in an `lc!` macro.
-//!
-//! Check the output binary using the `strings` command, e.g:
-//!
-//! ```bash
-//! ❯ strings target/debug/my_valuable_app | grep Voldemort
-//! ```
-//!
-//! If the output is blank then the resp. strings in your app are safe from a static analyzer tool
-//! like a hex editor.
-//!
-//! For an example see the `./examples` directory:
-//!
-//! ```bash
-//! ❯ cargo run --example simple
-//! ```
+//! 
+//! `use_litcrypt!` macro call should be called first for initialization before you can
+//! use `lc!` macro function. 
+//! 
+//! Please take note that you can set your encryption key to a specific value using the environment variable 
+//! `LITCRYPT_ENCRYPT_KEY` before compile. In case that you don't set this environment variable, the crate
+//! will generate a random encryption key at compilation time:
+//! e.g:
+//! 
+//!     $ export LITCRYPT_ENCRYPT_KEY="myverysuperdupermegaultrasecretkey"
+//! 
+//! Litcrypt will encrypt each string written inside `lc!` statically.
+//! 
+//! Check the output binary using `strings` command to verify:
+//! 
+//!     $ strings target/debug/my_valuable_app | grep Voldemort
+//! 
+//! If the output is blank then your valuable string in your app is safe from static analyzer tool
+//! like Hexeditor etc.
+//! 
+//! For working example code see `./examples` directory, and test using:
+//! 
+//!     $ cargo run --example simple
+
 extern crate proc_macro;
 extern crate proc_macro2;
 extern crate rand;
