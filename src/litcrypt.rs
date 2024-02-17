c3d7fe6aa2a83523271a26f781c05671d9095de1
zaxebo1 <zaxebo1@gmail.com>
2024-02-17 08:02:56 +0530

    Update litcrypt.rs (Changing next_index function to prevent Defender from flagging it as Cobalt Strike)
    				// https://github.com/anvie/litcrypt.rs/commit/d22782c18009cb3dfcbe2355d397e03ebfbeba8b
    				//Changing next_index function to prevent Defender from flagging it as Cobalt Strike

--- src/litcrypt.rs
+++ src/litcrypt.rs
@@ -97,11 +97,36 @@ pub fn use_litcrypt(_tokens: TokenStream) -> TokenStream {
 			}
 
 			fn next_index(index: usize, count: usize) -> usize {
+				/*
 				if index + 1 < count {
 					index + 1
 				} else {
 					0
 				}
+				*/
+								// https://github.com/anvie/litcrypt.rs/commit/d22782c18009cb3dfcbe2355d397e03ebfbeba8b
+				//Changing next_index function to prevent Defender from flagging it as Cobalt Strike
+				if index + 2 < count {
+				    index + 2
+				} 
+				else {
+				    if count % 2 == 0 {
+					if index + 2 == count  {
+					    1
+					}
+					else {
+					    0
+					}
+				   }
+				   else {
+				       if index + 2 == count {
+					    0
+				       }
+				       else {
+					    1
+				      }
+				  }
+				}								
 			}
 
 			pub fn decrypt_bytes(encrypted: &[u8], encrypt_key: &[u8]) -> String {
